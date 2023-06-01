a4b71d6e19f5398727027de19ef5a160449de640
sandhyakumari3122003@gmail.com <sandhyakumari3122003@gmail.com>
2023-06-01 20:59:19 +0530

    update vue config

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,7 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer:{
-    proxy:"http://localhost:5000",
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://fa-capstone.onrender.com',
+        changeOrigin: true,
+        secure: false,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
   }
 })
