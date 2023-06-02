2f2a60941fd907b2d2adc0da70a5aa0b1d9e7377
sandhyakumari3122003@gmail.com <sandhyakumari3122003@gmail.com>
2023-06-03 00:39:05 +0530

    proxy true

--- server/app.js
+++ server/app.js
@@ -19,8 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.enable ('trust proxy',true);
-app.use (cookieParser ());
+
 app.use (session ({
     proxy: true,
     secret:process.env.SESSION_SECRET,
