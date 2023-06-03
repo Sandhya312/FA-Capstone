b399772b3713bf718872b73a0b33c60ccb487ece
sandhyakumari3122003@gmail.com <sandhyakumari3122003@gmail.com>
2023-06-03 15:12:03 +0530

    changes

--- server/app.js
+++ server/app.js
@@ -19,14 +19,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.set('trust proxy', 1) // trust first proxy
+
 app.use (session ({
     proxy: true,
     secret:process.env.SESSION_SECRET,
-    resave: false,
+    resave: true,
     saveUninitialized: true,
-     cookie: { secure: true, httpOnly: false,
-        sameSite: 'none' }
    
 }));
 
