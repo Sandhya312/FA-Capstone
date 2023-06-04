17f15bf858f02bb7355a824dd01d9bf1ad945d20
sandhyakumari3122003@gmail.com <sandhyakumari3122003@gmail.com>
2023-06-04 22:11:41 +0530

    handle invalid login

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -61,7 +61,7 @@ export default {
         });
         console.log(response.data);
         // localStorage.setItem('userId',response.data);
-        this.$router.push({ path:`/user/${response.data}`});
+        this.$router.push({ path:`/user/${response.data[0]}`});
       } catch (err) {
         console.log(err);
       }
