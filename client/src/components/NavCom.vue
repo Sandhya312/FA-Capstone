0e46e2664b2ee4b904ec018768234a110a12e3ff
sandhyakumari3122003@gmail.com <sandhyakumari3122003@gmail.com>
2023-06-11 00:36:49 +0530

    improve navCom view

--- client/src/components/NavCom.vue
+++ client/src/components/NavCom.vue
@@ -1,87 +1,123 @@
 <template>
-    <div>
-      <nav class="navbar navbar-expand-lg ">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/">Job Portal</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <router-link to="/">Home</router-link>
-        </li>
-        <li class="nav-item login" v-if="isUser">
-          <router-link :to="`/user/${isUser}`">My Profile</router-link> 
-          <button @click="logout">Logout</button>
-        </li>
-        <li class="nav-item login" v-else>
-          <router-link to="/register">Register</router-link> 
-          <router-link  to="/login">Login</router-link> 
-        </li>
-      </ul>
-     
-    </div>
+  <div>
+    <nav class="navbar navbar-expand-lg">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/">Job Portal</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link to="/">Home</router-link>
+            </li>
+            <li class="nav-item login" v-if="isUser">
+         
+              <div class="dropdown">
+                <button
+                  class="btn btn-secondary dropdown-toggle"
+                  type="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  MyProfile
+                </button>
+                <ul class="dropdown-menu">
+                  <li><p>Personal</p></li>
+                  <li><router-link :to="`/user/${isUser}`">My Profile</router-link></li>
+                  <li><router-link :to="`/user/${isUser}/edit`">Edit Profile</router-link></li>
+                  <li>
+                    <router-link v-if="!user.is_emp" :to="`/user/${isUser}/applied`">Applied Jobs</router-link>
+                    <router-link v-else :to="`/user/${isUser}/myjobs`">My Jobs</router-link>
+
+                  </li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><p>Support</p></li>
+                  <li>  <button @click="logout" class="logout">Logout</button></li>
+                </ul>
+              </div>
+             
+            
+            </li>
+            <li class="nav-item login" v-else>
+              <router-link to="/register">Register</router-link>
+              <router-link to="/login">Login</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <router-view @login="onLogin" />
   </div>
-</nav>
-
-    <router-view @login="onLogin"/>
-
-    </div>
-
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
-  
-    name:'NavCom',
-    data() {
+  name: "NavCom",
+  data() {
     return {
-      isUser: localStorage.getItem('userId'),
-
-
-
-    }
+      isUser: localStorage.getItem("userId"),
+      user:{},
+    };
   },
-  methods:{
-    logout() {
-      axios.get('https://fa-capstone.onrender.com/user/logout')
+  methods: {
+    async logout() {
+     await axios
+        .get("/user/logout")
         .then(() => {
-          this.isUser='';
-         localStorage.clear();
-          this.$router.push('/login');
+          this.isUser = "";
+          localStorage.clear();
+          this.$router.push("/login");
         })
-        .catch(error => {
+        .catch((error) => {
           console.error(error);
         });
     },
-    onLogin(userId){
-     this.isUser=userId;
-
-  }
-
+    async getUser() {
+      try {
+        const id = this.isUser;
+        const response = await axios.get(`/user/${id}`);
+        this.user = response.data;
+        console.log("navbar",this.user);
+    
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    onLogin(userId) {
+      this.isUser = userId;
+    },
   },
- 
-}
+  mounted() {
+    this.getUser();
+  },
+};
 </script>
 
 <style scoped>
-
 nav {
   width: 80%;
   padding: 30px;
   position: relative;
   left: 10rem;
-  font-family: 'DM Sans', sans-serif;
-
+  font-family: "DM Sans", sans-serif;
 }
-.navbar-nav{
+.navbar-nav {
   margin: 0 50px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
 }
 /* .navbar-nav li a.router-link{
   text-decoration: none !important;
@@ -90,25 +126,40 @@ nav a {
   font-weight: bold;
   color: #2c3e50;
   text-decoration: none !important;
-
 }
-.login{
+.login {
   width: 50%;
-    display: flex;
-    justify-content: space-evenly;
+  display: flex;
+  justify-content: space-evenly;
 }
-.login a, .login button{
-  border: 2px solid black;
-    width: 120px;
-    display: flex;
-    /* text-align: center; */
-    border-radius: 20rem;
-    height: 41px;
-    align-items: center;
-    justify-content: center;
+.login a,
+.login button {
+  /* border: 2px solid black; */
+  width: 120px;
+  display: flex;
+  /* text-align: center; */
+  border-radius: 20rem;
+  height: 41px;
+  align-items: center;
+  justify-content: flex-start;
 }
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.dropdown-menu.show{
+  min-width: 256px !important;
+    display: flex;
+    flex-direction: column;
+    padding: 14%;
+    text-align: left;
+}
+.dropdown-menu li p {
+  color: gray !important;
+}
+.logout{
+  border: none !important;
+    background: transparent !important;
 }
 </style>
